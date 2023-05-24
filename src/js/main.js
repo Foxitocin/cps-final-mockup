fc2b7b39aee330637356faa564e5adbed9de0e68
Victoria Gorbunova <h.vic.alex@gmail.com>
2023-05-24 19:13:26 +0300

    stable version

--- src/js/main.js
+++ src/js/main.js
@@ -2,42 +2,52 @@ import '../../node_modules/focus-visible/dist/focus-visible';
 import '../scss/main.scss';
 import '../index.html';
 
-const buttonShow = document.querySelectorAll('.button-show');
+const buttonShow = document.querySelectorAll('.btn-show');
 
 function toggleButtonShow (event) {
-console.log(event.target);
-  const button = event.target.parentElement; //родитель элемента, на котором сработало событие
+let svg, span;
+const current = event.target;
+const button = current.parentElement; //родитель элемента, на котором сработало событие
+const textBox = button.previousElementSibling; //сосед кнопки, бокс с текстом, его будем обрезать после клика
 
-if (event.target.matches('span')) {
-  const sibling = event.target.previousElementSibling;//иконка svg, которую переворачиваем
-} else {
-  const svgParent = event.target.parentElement;
-  console.log(svgParent);
-  const sibling = event.target.nextElementSibling;
-  console.log(sibling);
-  const text = sibling.textContent;
-  console.log(text);
+if (current.matches('span')) {
+  //если кликнули на текст, то ищем иконку svg
+  svg = current.previousElementSibling;//иконка svg, которую перевернем
+  toggleButton(current, svg);
+} else { //в противном случае начинаем искать от svg
+  svg = current;
+  span = svg.nextElementSibling; //находим span с текстом
+  toggleButton(span, svg);
 };
-  //  const buttonSibling = button.previousElementSibling; //сосед кнопки, бокс с текстом
-
-  //  buttonSibling.classList.toggle('_hide');
-  //   if (buttonSibling.classList.contains('_hide') === false){
-  //      event.target.textContent = 'Скрыть';
-  //      sibling.classList.add('_rotate');
-  //   } else {
-  //      event.target.textContent = 'Читать далее';
-  //      sibling.classList.remove('_rotate');
-  //   }
+//вспомогательная функция
+function toggleButton (text, svg) {
+  textBox.classList.toggle('_hide');
+  if (textBox.classList.contains('_hide') === false) {
+     text.textContent = 'Скрыть';
+     svg.classList.add('_rotate');
+ 
+  } else {
+    if (textBox.parentElement.classList.contains('about__text')) {
+      text.textContent = 'Читать далее';
+      svg.classList.remove('_rotate');
+ 
+    } else {
+      text.textContent = 'Показать все';
+      svg.classList.remove('_rotate');
+  
+    }
+  }
+}
 }
 
-buttonShow.forEach (( item, index ) => {
+buttonShow.forEach(( item, index ) => {
   buttonShow[index].addEventListener ('click', toggleButtonShow);
 });
 
 
-
+// Swiper
 let swiper = undefined;
-let init= false;
+let init = false;
 
 function initSwiper() {
   if (window.innerWidth <= 768) {
@@ -58,47 +68,10 @@ function initSwiper() {
         });
       };
   } else if (init) {
-    const containers = document.querySelectorAll('.swiper-container');
-    let wrappers = [];
-    let items = [];
-    let pagination = [];
-    containers.forEach ((elem, index) => {
-      let wrapper = containers[index].querySelectorAll('.swiper-wrapper');
-      wrappers.push(wrapper);
-      console.log(wrappers);
-      let item = containers[index].querySelectorAll('.swiper-item');
-      items.push(item);
-      let page = containers[index].querySelectorAll('.swiper-pagination');
-      pagination.push(page);
-    });
-    removeClass(pagination,'swiper-pagination');
-    removeClass(items,'swiper-item');
-    removeClass(wrappers,'swiper-wrapper');
-    removeClass(containers,'swiper-container');
-
-    // if (wrapper.classList.contains('swiper-wrapper')) {
-    //   wrapper.classList.remove('swiper-wrapper');
-    //   console.log('Wrapper remove');
-    // }
-    // if (item.classList.contains('swiper-item')) {
-    //   item.classList.remove('swiper-item');
-    //   console.log('Item remove');
-    // }
-    // if (page.classList.contains('swiper-pagination')) {
-    //   page.classList.remove('swiper-pagination');
-    //   console.log('Pagination remove');
-    // }
-    // console.log(wrappers, items, pagination);
+    if ( swiper !== undefined ) swiper.destroy( true, true );
     };
   }
-  function removeClass (elems, value) {
-    elems.forEach((elem, index) => {
-      let flag = elems[index].classList.contains(`${value}`);
-      if (flag) {
-        elems[index].classList.remove(`${value}`);
-        console.log(`${value} remove`);
-      }
-      });
-  };
+
 document.addEventListener("DOMContentLoaded", initSwiper);
 window.addEventListener("resize", initSwiper);
+
